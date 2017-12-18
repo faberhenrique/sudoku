75b3b9bc8763b557adec997cb8c00651f4d23443
faberhenrique <faber.henrique@gmail.com>
2017-12-18 13:51:38 -0200

    Util udacity update

--- util_udacity.py
+++ util_udacity.py
@@ -1,23 +1,94 @@
+
+
 rows = 'ABCDEFGHI'
 cols = '123456789'
+boxes = [r + c for r in rows for c in cols]
+history = {}  # history must be declared here so that it exists in the assign_values scope
 
-def cross(a, b):
-    return [s+t for s in a for t in b]
+def assign_value(values, box, value):
+    """You must use this function to update your values dictionary if you want to
+    try using the provided visualization tool. This function records each assignment
+    (in order) for later reconstruction.
 
-boxes = cross(rows, cols)
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
 
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+    Returns
+    -------
+    dict
+        The values dictionary with the naked twins eliminated from peers
+    """
+    # Don't waste memory appending actions that don't actually change any values
+    if values[box] == value:
+        return values
+
+    prev = values2grid(values)
+    values[box] = value
+    if len(value) == 1:
+        history[values2grid(values)] = (prev, (box, value))
+    return values
+
+def cross(A, B):
+    """Cross product of elements in A and elements in B """
+    return [x+y for x in A for y in B]
+
+
+def values2grid(values):
+    """Convert the dictionary board representation to as string
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    """
+    res = []
+    for r in rows:
+        for c in cols:
+            v = values[r + c]
+            res.append(v if len(v) == 1 else '.')
+    return ''.join(res)
+
+
+def grid2values(grid):
+    """Convert grid into a dict of {square: char} with '123456789' for empties.
+
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    
+    Returns
+    -------
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value,
+            then the value will be '123456789'.
+    """
+    sudoku_grid = {}
+    for val, key in zip(grid, boxes):
+        if val == '.':
+            sudoku_grid[key] = '123456789'
+        else:
+            sudoku_grid[key] = val
+    return sudoku_grid
+
 
 def display(values):
-    """
-    Display the values as a 2-D grid.
-    Input: The sudoku in dictionary form
-    Output: None
+    """Display the values as a 2-D grid.
+
+    Parameters
+    ----------
+        values(dict): The sudoku in dictionary form
     """
     width = 1+max(len(values[s]) for s in boxes)
     line = '+'.join(['-'*(width*3)]*3)
@@ -25,68 +96,31 @@ def display(values):
         print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
                       for c in cols))
         if r in 'CF': print(line)
-    return
+    print()
 
-def grid_values(grid):
-    """
-    Convert grid into a dict of {square: char} with '123456789' for empties.
-    Input: A grid in string form.
-    Output: A grid in dictionary form
-            Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
-    """
-    chars = []
-    digits = '123456789'
-    for c in grid:
-        if c in digits:
-            chars.append(c)
-        if c == '.':
-            chars.append(digits)
-    assert len(chars) == 81
-    return dict(zip(boxes, chars))
-    
-def eliminate(values):
-    """
-    Go through all the boxes, and whenever there is a box with a value, eliminate this value from the values of all its peers.
-    Input: A sudoku in dictionary form.
-    Output: The resulting sudoku in dictionary form.
-    """
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    for box in solved_values:
-        digit = values[box]
-        for peer in peers[box]:
-            values[peer] = values[peer].replace(digit,'')
-    return values
 
-def only_choice(values):
-    """
-    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
-    Input: A sudoku in dictionary form.
-    Output: The resulting sudoku in dictionary form.
-    """
-    for unit in unitlist:
-        for digit in '123456789':
-            dplaces = [box for box in unit if digit in values[box]]
-            if len(dplaces) == 1:
-                values[dplaces[0]] = digit
-    return values
+def reconstruct(values, history):
+    """Returns the solution as a sequence of value assignments 
 
-def reduce_puzzle(values):
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    history(dict)
+        a dictionary of the form {key: (key, (box, value))} encoding a linked
+        list where each element points to the parent and identifies the value
+        assignment that connects from the parent to the current state
+
+    Returns
+    -------
+    list
+        a list of (box, value) assignments that can be applied in order to the
+        starting Sudoku puzzle to reach the solution
     """
-    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
-    If the sudoku is solved, return the sudoku.
-    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
-    Input: A sudoku in dictionary form.
-    Output: The resulting sudoku in dictionary form.
-    """
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    stalled = False
-    while not stalled:
-        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
-        values = eliminate(values)
-        values = only_choice(values)
-        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
-        stalled = solved_values_before == solved_values_after
-        if len([box for box in values.keys() if len(values[box]) == 0]):
-            return False
-    return values
+    path = []
+    prev = values2grid(values)
+    while prev in history:
+        prev, step = history[prev]
+        path.append(step)
+    return path[::-1]
